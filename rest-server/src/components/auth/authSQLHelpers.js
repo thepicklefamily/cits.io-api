243fc83647bbb0c01c01dbde41b7bbcd62f9e97f
Jaehyun Chi <jaehchi@gmail.com>
2018-01-29 17:33:12 -0800

    [add] - signuphelper

--- rest-server/src/components/auth/authSQLHelpers.js
+++ rest-server/src/components/auth/authSQLHelpers.js
@@ -1,7 +1,9 @@
-export const signUpHelper = () => {
-  
+export const signUpHelper = ({id, email, username, password, type}) => {
+  return `
+    INSERT into Users (id, email, username, password, type)
+    VALUES ('${id}','${email}','${username}','${password}','${type}')
+  `
 }
-
 export const loginHelper = ({ username }) => {
   return `
     SELECT id, email, username, password, type
