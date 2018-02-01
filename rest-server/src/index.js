02578e49e476961fcf539aec0125ca3d3086a4cb
Avi Cohen <acemail10@gmail.com>
2018-01-31 19:39:37 -0800

    [test] more testing

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -10,9 +10,16 @@ const app = App.express;
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
+
+app.use((req, res, next) => {
+  console.log('meow', req.method, req.statusCode);
+
+})
+
 server.listen(PORT, (err) => {
   if (err) throw new Error;
   success('successfully connected to port ', PORT);
 });
 
 module.exports.app = app;
+module.exports.server = server;
