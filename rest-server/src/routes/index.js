3bd29eab15a641e9a0d793de4cb2ad7c3b6a644f
Sylvain Ung <sylvainung@live.com>
2018-01-31 16:05:06 -0800

    Implements route to add property to DB

--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import authRouter from '../components/auth/authRouter';
+import propertyRouter from '../components/property/propertyRouter';
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.use('/auth', authRouter);
 // router.use('/posts', postRouter);
 // router.use('/articles', articleRouter);
 // router.use('/phonebooks', phonebookRouter);
-// router.use('/properties', propertyRouter);
+router.use('/properties', propertyRouter);
 // router.use('/tenantTickets', tenentTicketRouter);
 // router.use('userTickets', userTicketRouter);
 // router.use('propertyTickets', propertyTicketRouter);
