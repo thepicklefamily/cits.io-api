5a0584f8f7df73df4ffe6e49e2c5c53a453f3f7f
David Darley <daviddar232@gmail.com>
2018-02-01 16:23:09 -0800

    [ops] - adding the client/serverEvents to socket-server

--- socket-server/src/serverEvents.js
+++ socket-server/src/serverEvents.js
@@ -1,3 +1,6 @@
 const serverInitialState = ({ client, room }, payload) => {
-  
+  console.log('server initial state');
+  client.emit('server.initialState', alert('hello'));
 }
+
+module.exports = { serverInitialState };
