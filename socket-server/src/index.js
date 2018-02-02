587f598aaa3f51a01d3d1a904505991669b6fc2c
David Darley <daviddar232@gmail.com>
2018-02-02 15:47:06 -0800

    [edit] - deleted the commented out code on index.js in socket-server

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -1,31 +1,3 @@
-// const http = require('http');
-// const SocketIo = require('socket.io');
-// const { each } = require('lodash');
-
-// const log = require('./lib/log');
-// const Rooms = require('./rooms');
-// const clientEvents = require('./clientEvents');
-
-// const server = http.createServer();
-// const io = SocketIo(server);
-// const rooms = new Rooms(io);
-
-// io.on('connection', (client) => {
-//   log('client connected');
-//   const { roomId } = client.handshake.query;
-//   const room = rooms.findOrCreate(roomId || 'default');
-//   client.join(room.get('id'));
-
-//   each(clientEvents, (handler, event) => {
-//     client.on(event, handler.bind(null, { io, client, room }));
-//   });
-// })
-
-// server.listen(PORT, () => {
-//   log(`socket server listening on port ${PORT}`);
-// });
-
-
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -41,7 +13,6 @@ io.on('connection', (socket) => {
   console.log('user connected');
   const { roomId } = socket.handshake.query;
   const room = rooms.findOrCreate(roomId || 'default');
-  console.log(room);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   })
