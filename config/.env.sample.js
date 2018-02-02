ab8a7a0733f20d8dacc50c7b47197455753a92f4
Avi Cohen <acemail10@gmail.com>
2018-02-01 19:29:02 -0800

    [add] update env.sample.js # Please enter the commit message for your changes. Lines starting

--- config/.env.sample.js
+++ config/.env.sample.js
@@ -1,18 +1,20 @@
 const envBuild = {
- 'rest-server': [
-   'PORT=3396',
-   'LOCAL_USER=root',
-   'LOCAL_HOST=localhost',
-   'LOCAL_DATABASE=cits',
-   'LOCAL_PASSWORD=',
-   'LOCAL_PORT=5432',
-   'AWS_USER=',
-   'AWS_HOST=',
-   'AWS_DATABASE=',
-   'AWS_PASSWORD=',
-   'AWS_PORT=',
-   'SALT_ROUNDS=10',
-   'TOKEN_SECRET=citsapi'
+  'rest-server': [
+    'DEBUG=TRUE',
+    'NODE_ENV=test',
+    'PORT=3396',
+    'LOCAL_USER=root',
+    'LOCAL_HOST=localhost',
+    'LOCAL_DATABASE=cits',
+    'LOCAL_PASSWORD=',
+    'LOCAL_PORT=5432',
+    'AWS_USER=',
+    'AWS_HOST=',
+    'AWS_DATABASE=',
+    'AWS_PASSWORD=',
+    'AWS_PORT=',
+    'SALT_ROUNDS=10',
+    'TOKEN_SECRET_KEY=citsapi'
  ]
 };
 
