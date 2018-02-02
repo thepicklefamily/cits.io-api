a0bc1cecd12137d7303fdd71cda438e622e64c3e
Jaehyun Chi <jaehchi@gmail.com>
2018-02-01 22:21:30 -0800

    [add] - adding sql helpers and queries for articles

---
+++ rest-server/src/components/articles/articlesControllers.js
@@ -0,0 +1,40 @@
+import db from "../../config/database";
+import {
+  addArticleQuery,
+  editArticleQuery,
+  deleteArticleQuery,
+  fetchAllArticlesQuery
+} from "./articlesQueries";
+import { sucesss, error } from '../../lib/log';
+
+export const addArticleController = async (req, res) => {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+export const editArticleController = async (req, res) => {
+  try {
+
+  } catch (err) {
+    
+  }
+}
+
+export const deleteArticleController = async (req, res) => {
+  try {
+
+  } catch (err) {
+    
+  }
+}
+
+export const fetchAllArticlesController = async (req, res) => {
+  try {
+
+  } catch (err) {
+    
+  }
+}
---
+++ rest-server/src/components/articles/articlesSQLHelpers.js
@@ -0,0 +1,28 @@
+export const addArticleHelper = ({ title, content, date, userId,}) => {
+  return `
+    INSERT INTO articles (title, content, date, userId, propertyId) 
+    VALUES ('${title}','${content}','${date}','${userId}','${propertyId}')
+    RETURNING id, title, content, date
+  `
+}
+
+export const editArticleHelper = ({ title, content, date, article_id }) => {
+  return `
+  UPDATE articles SET title = '${title}', content = '${content}', date = '${date}' 
+  WHERE id = ${article_id}
+  RETURNING id, title, content, date
+`
+}
+
+export const deleteArticleHelper = ({ article_id }) => {
+  return `
+    DELETE FROM articles
+    WHERE id = ${article_id}
+  `
+}
+
+export const fetchAllArticlesHelper = ({ }) => {
+  return `
+    SELECT * FROM articles
+  `
+}
