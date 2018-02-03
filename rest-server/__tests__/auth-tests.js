379218be0c3fc213829b101a693c4a569de23cde
Avi Cohen <acemail10@gmail.com>
2018-02-03 10:35:18 -0800

    [test] updated auth test

--- rest-server/__tests__/auth-tests.js
+++ rest-server/__tests__/auth-tests.js
@@ -1,4 +1,4 @@
-const request = require('supertest');
+import request from 'supertest';
 import server from '../src/index.js';
 
 //to drop and create tables - SHOULD MATCH WHAT'S IN src\config\database\setup.js but without "process.exit()""
