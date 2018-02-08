267eb473ba179fff5eb0aee7d37a86bda7ab8335
David Darley <daviddar232@gmail.com>
2018-02-07 16:34:50 -0800

    [ops] - adding gmail as the service for the smtp server

--- smtp-server/index.js
+++ smtp-server/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const router = require('./routes');
+const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use('/tickets', router);
 
--- smtp-server/routes/ticketController.js
+++ smtp-server/routes/ticketController.js
@@ -1,6 +1,45 @@
 const TicketEmailController = {
-  sendTicketEmail: () => {
+  sendTicketEmail: (req, res) => {
+    const output = `
+      <p>You have a new tenant ticket.</p>
+      <h3>Ticket Details</h3>
+      <ul>
+        <li>Name: CHANGE TO req.body.name</li>
+        <li>Email: CHANGE TO req.body.email</li>
+        <li>Phone: CHANGE TO req.body.phone</li>
+      </ul>
+      <a href="http://localhost:3000">CHANGE TO ticket link</a>
+    `;
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      service: 'gmail',
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: account.user, // generated ethereal user
+        pass: account.pass  // generated ethereal password
+      }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"Castle in the Sky Ticket Info" <whatever is in auth.user>', // sender address
+      to: 'daviddar232@gmail.com', // list of receivers
+      subject: 'New Tenant Ticket', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
     console.log('sending ticket!');
+    res.status(200).send('sup?');
   }
 }
 
