8f766969adb7d0af3ba6b875550c4fc0f1bd43cb
Patrick <pkwu@berkeley.edu>
2018-02-09 10:38:56 -0800

    [Dev]

--- rest-server/src/middleware/auth/jwt.js
+++ rest-server/src/middleware/auth/jwt.js
@@ -5,7 +5,8 @@ import { success, error } from '../../lib/log';
 export const generateToken = async (username, id) => { 
   const token = {};
   const payload = {
-    exp: (Date.now() / 1000) + 604800,
+    // exp: (Date.now() / 1000) + 604800,
+    exp: (Date.now() / 1000) + 30,
     username, 
     id
   }
