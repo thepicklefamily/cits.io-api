9c659d00b8d943df8e93177cd87d0cf2fb38bfc2
Avi Cohen <acemail10@gmail.com>
2018-02-09 19:32:10 -0800

    [add] start setting up chat notifications

--- socket-server/config/mongo.js
+++ socket-server/config/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect('mongodb://localhost/messages');
+mongoose.connect('mongodb://localhost/socketdb');
 
+//Messages table:
 const messageSchema = new Schema({
   message: String,
   username: String,
@@ -11,6 +12,20 @@ const messageSchema = new Schema({
   date: { type: Date, default: Date.now }
 });
 
+
 const Messages = mongoose.model('Messages', messageSchema);
 
+
+//Time last online in property chat room table:
+const lastOnlineSchema = new Schema({
+  userId: String,
+  propId: String,
+  timeLastOnline: String
+});
+
+
+const lastOnline = mongoose.model('lastOnline', lastOnlineSchema);
+
+
 module.exports.Messages = Messages;
+module.exports.lastOnline = lastOnline;
--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+export const io = require('socket.io')(server);
 const { each } = require('lodash');
 const Rooms = require('./rooms');
 const { Messages } = require('../config/mongo');
@@ -8,7 +8,35 @@ const rooms = new Rooms(io);
 const clientEvents = require('./clientEvents');
 const PORT = process.env.PORT || 4155;
 const redisDB = require('../config/redis');
+import notificationEmitters from './chatNotifications/index.js';
 
+
+
+//once get this all working, figure out how to modularize more and get into dif files, clean up extra junk, etc.
+
+//Chat Notifications Socket:
+const chatNotificationSocket = io.of('/chat-notifications');
+chatNotificationSocket.on('connection', function(socket){
+  console.log('a user connected to chat-notifications socket');
+
+  chatNotificationSocket.emit('initial.notifications', 'everyone!');
+
+  socket.on('notifications.ready', (data) => {
+    console.log('received user id', data);
+    // look up the appropriate notifs and then send back an array of prop ids to have notifs on.
+    let arrayOfPropsforNotifs = [1, 2, 3, 4];
+    io.emit('initial-notifications', arrayOfPropsforNotifs)  
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected from chat-notifications socket');
+  });
+});
+
+
+//david's stuff below:
+
+//Chat Messaging Socket:
 io.on('connection', (socket) => {
   console.log('user connected');
   const { roomId } = socket.handshake.query;
