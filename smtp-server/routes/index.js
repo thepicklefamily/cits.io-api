93a0010bdc075acdb9648d7148bfcc692d0d2f63
David Darley <daviddar232@gmail.com>
2018-02-15 16:51:42 -0800

    [ops] - adding full route for sending welcome email.

--- smtp-server/routes/index.js
+++ smtp-server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const TicketEmailController = require('./ticketController');
+const WelcomeEmailController = require('./welcomeController');
 
 router.route('/sendTicketEmail')
   .post(TicketEmailController.sendTicketEmail)
-
+router.route('/sendWelcomeEmail')
+  .post(WelcomeEmailController.sendWelcomeEmail)
 module.exports = router;
